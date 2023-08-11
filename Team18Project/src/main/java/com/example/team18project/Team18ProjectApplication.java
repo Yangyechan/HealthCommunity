02864c2f722999d21762f6aec08d981aaf759add
baejeu12 <baejeu@naver.com>
2023-08-11 22:38:22 +0900

    프로젝트 시작/ main branch 생성

---
+++ Team18Project/src/main/java/com/example/team18project/Team18ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.team18project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team18ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team18ProjectApplication.class, args);
+    }
+
+}
---
+++ Team18Project/src/test/java/com/example/team18project/Team18ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team18project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team18ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
