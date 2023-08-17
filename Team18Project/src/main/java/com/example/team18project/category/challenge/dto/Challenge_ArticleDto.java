b9d33720468853971dfac227c2af80d0bb84455d
yechanYang <yac392045@mokpo.ac.kr>
2023-08-17 17:43:26 +0900

    feat: challenge 게시글 작성 기능 구현

---
+++ Team18Project/src/main/java/com/example/team18project/category/challenge/dto/Challenge_ArticleDto.java
@@ -0,0 +1,27 @@
+package com.example.team18project.category.challenge.dto;
+
+import com.example.team18project.category.challenge.entities.Challenge_ArticleEntity;
+import com.example.team18project.category.user.entities.UserEntity;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class Challenge_ArticleDto {
+    private String nickname;
+
+    private String title;
+
+    private String content;
+
+    private LocalDateTime created_at;
+
+    public static Challenge_ArticleDto fromEntity(Challenge_ArticleEntity entity) {
+        Challenge_ArticleDto dto = new Challenge_ArticleDto();
+        dto.setNickname(entity.getUser().getNickname());
+        dto.setTitle(entity.getTitle());
+        dto.setContent(entity.getContent());
+        dto.setCreated_at(entity.getCreated_at());
+        return dto;
+    }
+}
